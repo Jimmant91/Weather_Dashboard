3d9bac23d5986a77b216761ed1ae722be4089b67
James Antley <jimmy.antley@gmail.com>
2021-05-10 00:23:00 -0400

    removed unecessary classes in html

--- assets/script.js
+++ assets/script.js
@@ -8,18 +8,6 @@ var windSpeed = $("#wind-speed");
 var uvIndex = $("#uv-index");
 var iconElement = $(".icon");
 
-// currentCity.innerHTML =
-
-// temperature.innerHTML =
-
-// humidity.innerHTML =
-
-// windSpeed.innerHTML =
-
-// uvIndex.innerHTML =
-
-// iconElement.innerHTML =
-
 var handleFormSubmit = function (event) {
     event.preventDefault();
 
@@ -50,7 +38,7 @@ function getApi() {
 
             
 
-            var cityText = $("#current-city").innerHTML;
+            var cityText = $("#current-city")
             cityText.text(data.name + ' ' + `(${cityDate}) `);
             cityText.append(weatherIcon);
 
@@ -101,4 +89,63 @@ function getApi() {
         // renderFutureForecast();
             
         })
+}
+
+function renderFutureForecast() {
+
+    var cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    fetch(cityUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        //console.log(data);
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+
+        var futureForecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,hourly,minutely&appid=${apiKey}`;
+            
+        fetch (futureForecastUrl)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                //console.log(data);
+                var weatherCardContainer = $("#weather-cards");
+                weatherCardContainer.empty();
+                //console.log(data.daily.length);
+                for (var i=0; i<5; i++) {
+                    var weatherCard = document.createElement("div");
+                    weatherCard.setAttribute("class", "col-12 col-xl border border-dark mx-2 forecast-bg text-white");
+                    weatherCardContainer.append(weatherCard);
+
+                    var weatherCardDate = document.createElement("div");
+                    weatherCardDate.setAttribute("class", "has-text-white has-text-centered is-size-4")
+                    weatherCardDate.textContent = moment.unix(data.daily[i].dt).format("M/D/yyyy");
+                    weatherCard.append(weatherCardDate);
+
+                    var weatherIconContainer = document.createElement("div");
+                    weatherIconContainer.setAttribute("class", "is-flex is-justify-content-center");
+                    weatherCard.append(weatherIconContainer);
+
+                    var weatherCardIcon = document.createElement("img");
+                    var weatherIconCode = data.daily[i].weather[0].icon;
+                    weatherCardIcon.setAttribute("class", "weather-icon");
+                    weatherCardIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`);
+                    weatherIconContainer.append(weatherCardIcon);
+
+                    var weatherCardTemp = document.createElement("div");
+                    weatherCardTemp.setAttribute("class", "has-text-white has-text-centered");
+                    weatherCardTemp.textContent = "Temp: " + data.daily[i].temp.day + " °F";
+                    weatherCard.append(weatherCardTemp);
+
+                    var weatherCardHumidity = document.createElement("div");
+                    weatherCardHumidity.setAttribute("class", "has-text-white has-text-centered");
+                    weatherCardHumidity.textContent = "Humidity: " + data.daily[i].humidity + " %";
+                    weatherCard.append(weatherCardHumidity);
+                    
+                }
+            }) 
+    })
 }
