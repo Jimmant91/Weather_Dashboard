119b565a8f1c1b010e684e6c520c669d83a16a2b
James Antley <jimmy.antley@gmail.com>
2021-05-10 00:50:42 -0400

    restarting JS with basic api variables

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,4 @@
-var city = "Raleigh";
+var currentDay = moment().format(MM-DD-YYYY);
 var apiKey = "b563522ef21f105beb9c536cd2f379b4";
 var requestCityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-var cityInput = $("#city-input");
-var temperature = $("#temp");
-var humidity = $("#humidity");
-var windSpeed = $("#wind-speed");
-var uvIndex = $("#uv-index");
-
-var iconCurrent = data.current.weather[0].icon
-function dailyWeather() {
-    var currentIconURL = 'http://openweathermap.org/img/wn/'+iconCurrent+'@2x.png'
-    temperature.text(tempF.toFixed(2));
-    windspeed.text(windSpeed);
-    humidity.text(humidity);
-    uvIndex.text(uvIndex);
-    $('#date').text(city + '  '+date + ' ');
-
-}
-
-dailyWeather()
+var citiesArray = [];
